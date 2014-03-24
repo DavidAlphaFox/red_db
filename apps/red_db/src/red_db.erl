fedf6506d346c93a359a5c9126d1afa14acef754
David.Gao <david.alpha.fox@gmail.com>
2014-03-24 15:11:58 +0800

    add red_db file

---
+++ apps/red_db/src/red_db.erl
@@ -0,0 +1,48 @@
+-module(red_db).
+-behaviour(gen_server).
+-export([start_link/1]).
+-export([init/1, handle_call/3, handle_cast/2, handle_info/2, terminate/2, code_change/3]).
+
+-record(state, {
+        }).
+-type state() :: #state{}.
+
+
+%%% =================================================================================================
+%%% External functions
+%%% =================================================================================================
+-spec start_link(non_neg_integer()) -> {ok, pid()}.
+%%% @doc starts a new db client
+start_link(Index) ->
+  gen_server:start_link({local,db(Index)}, ?MODULE, Index, []).
+%%% =================================================================================================
+%%% Server functions
+%%% =================================================================================================
+%%% @hidden
+-spec init(non_neg_integer()) -> {ok, state()} | {stop, any()}.
+init(Index) ->
+	{ok,#state{}}.
+
+-spec db(non_neg_integer()) -> atom().
+db(Index) ->
+  list_to_atom("red_db_" ++ integer_to_list(Index)).
+handle_call(Any, _From, State) ->
+  {reply, {ok,"R"}, State}.
+
+handle_cast(_Any,State)->
+	{noreply,State}.
+%% @hidden
+-spec handle_info(term(), state()) -> {noreply, state(), hibernate}.
+handle_info(_, State) -> 
+	{noreply, State, hibernate}.
+
+%% @hidden
+-spec terminate(term(), state()) -> ok.
+terminate(_, _) -> 
+	ok.
+
+%% @hidden
+-spec code_change(term(), state(), term()) -> {ok, state()}.
+code_change(_OldVsn, State, _Extra) -> 
+	{ok, State}.
+
