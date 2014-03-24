84297ddcad5b80f59f5050ab0ffaef0a780c79e7
David.Gao <david.alpha.fox@gmail.com>
2014-03-24 19:20:53 +0800

    make redis protocol parser in single file

--- apps/red_db/src/redis_runner.erl
+++ apps/red_db/src/redis_runner.erl
@@ -1,7 +1,7 @@
 %%%-------------------------------------------------------------------                                                          
 %%% @author David.Gao <david.alpha.fox@gmail.com>                                                                                                                                     
 %%% @copyright (C) 2014                                                                                  
-%%% @doc redis protocol paser and runner                                                                               
+%%% @doc redis protocol runner                                                                               
 %%% @end                                                                                                                        
 %%%------------------------------------------------------------------
 -module(redis_runner).
@@ -55,11 +55,11 @@ handle_cast({run, Cmd, Args}, State) ->
   try
     OriginalCommand = #redis_command{cmd = Cmd,
                                     args = Args},
-    Command = parse_command(OriginalCommand),
+    Command = redis_parser:parse_command(OriginalCommand),
     run(Command,State)
   catch
     _:Error->
-    tcp_err(parse_error(Cmd, Error), State)
+    tcp_err(redis_parser:parse_error(Cmd, Error), State)
   end.
 
 
@@ -79,59 +79,6 @@ code_change(_OldVsn, State, _Extra) ->
 	{ok, State}.
 
 
-parse_error(Cmd, unsupported) -> <<Cmd/binary, " unsupported in this version">>;
-parse_error(Cmd, nested) -> <<Cmd/binary, " calls can not be nested">>;
-parse_error(Cmd, out_of_multi) -> <<Cmd/binary, " without MULTI">>;
-parse_error(Cmd, not_in_multi) -> <<Cmd/binary, " inside MULTI is not allowed">>;
-parse_error(_Cmd, db_in_multi) -> <<"Transactions may include just one database">>;
-parse_error(Cmd, out_of_pubsub) -> <<Cmd/binary, " outside PUBSUB mode is not allowed">>;
-parse_error(_Cmd, not_in_pubsub) -> <<"only (P)SUBSCRIBE / (P)UNSUBSCRIBE / QUIT allowed in this context">>;
-parse_error(Cmd, unknown_command) -> <<"unknown command '", Cmd/binary, "'">>;
-parse_error(_Cmd, no_such_key) -> <<"no such key">>;
-parse_error(_Cmd, syntax) -> <<"syntax error">>;
-parse_error(_Cmd, not_integer) -> <<"value is not an integer or out of range">>;
-parse_error(_Cmd, {not_integer, Field}) -> [Field, " is not an integer or out of range"];
-parse_error(_Cmd, {not_float, Field}) -> [Field, " is not a double"];
-parse_error(_Cmd, {out_of_range, Field}) -> [Field, " is out of range"];
-parse_error(_Cmd, {is_negative, Field}) -> [Field, " is negative"];
-parse_error(_Cmd, not_float) -> <<"value is not a double">>;
-parse_error(_Cmd, bad_item_type) -> <<"Operation against a key holding the wrong kind of value">>;
-parse_error(_Cmd, source_equals_destination) -> <<"source and destinantion objects are the same">>;
-parse_error(Cmd, bad_arg_num) -> <<"wrong number of arguments for '", Cmd/binary, "' command">>;
-parse_error(_Cmd, {bad_arg_num, SubCmd}) -> ["wrong number of arguments for ", SubCmd];
-parse_error(_Cmd, unauthorized) -> <<"operation not permitted">>;
-parse_error(_Cmd, nan_result) -> <<"resulting score is not a number (NaN)">>;
-parse_error(_Cmd, auth_not_allowed) -> <<"Client sent AUTH, but no password is set">>;
-parse_error(_Cmd, {error, Reason}) -> Reason;
-parse_error(_Cmd, Error) -> io_lib:format("~p", [Error]).
-
--spec parse_command(#redis_command{args :: [binary()]}) -> #redis_command{}.
-parse_command(C = #redis_command{cmd = <<"QUIT">>, args = []}) ->
- C#redis_command{result_type = ok, group=connection};
-parse_command(#redis_command{cmd = <<"QUIT">>}) -> 
-  throw(bad_arg_num);
-parse_command(C = #redis_command{cmd = <<"PING">>, args = []}) -> 
-  C#redis_command{result_type = string, group=connection};
-parse_command(#redis_command{cmd = <<"PING">>}) -> 
-  throw(bad_arg_num);
-parse_command(C = #redis_command{cmd = <<"DECR">>, args = [_Key]}) -> 
-  C#redis_command{result_type = number, group=strings};
-parse_command(#redis_command{cmd = <<"DECR">>}) ->
-  throw(bad_arg_num);
-parse_command(C = #redis_command{cmd = <<"DECRBY">>, args = [Key, Decrement]}) -> 
-  C#redis_command{args = [Key, binary_to_integer(Decrement)],result_type = number, group=strings};
-parse_command(#redis_command{cmd = <<"DECRBY">>}) -> 
-  throw(bad_arg_num);
-parse_command(C = #redis_command{cmd = <<"INCR">>, args = [_Key]}) -> 
-  C#redis_command{result_type = number, group=strings};
-parse_command(#redis_command{cmd = <<"INCR">>}) -> 
-  throw(bad_arg_num);
-parse_command(C = #redis_command{cmd = <<"INCRBY">>, args = [Key, Increment]}) -> 
-  C#redis_command{args = [Key, binary_to_integer(Increment)], result_type = number, group=strings};
-parse_command(#redis_command{cmd = <<"INCRBY">>}) -> 
-  throw(bad_arg_num);
-parse_command(C = #redis_command{cmd = Any}) -> 
-  C#redis_command{ result_type = string, group=strings}.
 
 run(#redis_command{cmd = <<"QUIT">>}, State) ->
   case tcp_ok(State) of
